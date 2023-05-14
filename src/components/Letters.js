6799210462102b6276cdcde2a783a4768f094e3b
MariaCLara <mariaclara130196@gmail.com>
2023-05-13 21:48:27 -0300

    TESTE PARA VER FUNCIONALIDADES REACT

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 export default function Letters () {
 
-    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     
-}
-
-return (
     
+    return (
+        <div className="container-letters">
 
-)
+        </div>
+
+    )
+}  
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom"
 import "../src/css/reset.css";
 import "../src/css/style.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"
 
 
 const root = App();
