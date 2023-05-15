9f0107df95a957214ac8d9fbd7169a4c7f386d7e
MariaCLara <mariaclara130196@gmail.com>
2023-05-15 18:52:27 -0300

    FINAL

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "../src/css/reset.css";
-import "../src/css/style.css";
-
-
-const root = App();
-ReactDOM.render(root, document.querySelector(".root"));
