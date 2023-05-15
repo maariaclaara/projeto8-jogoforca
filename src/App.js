489a2f3a91d8dcfef99d30d369e0546ebec1c5ff
MariaCLara <mariaclara130196@gmail.com>
2023-05-15 17:55:35 -0300

    APP.JS TERMINADO / IMAGENS NÃPO CARREGAM

--- src/App.js
+++ src/App.js
@@ -3,29 +3,27 @@ import Game from "./components/Game";
 import Letters from "./components/Letters";
 import palavras from "./palavras";
 
-import forca0 from "./assets/forca0.png";
-import forca1 from "./assets/forca1.png";
-import forca2 from "./assets/forca2.png";
-import forca3 from "./assets/forca3.png";
-import forca4 from "./assets/forca4.png";
-import forca5 from "./assets/forca5.png";
-import forca6 from "./assets/forca6.png";
+import forca0 from "./assets/image/forca0.png";
+import forca1 from ".assets/image/forca1.png";
+import forca2 from "./assets/image/forca2.png";
+import forca3 from "./assets/image/forca3.png";
+import forca4 from "./assets/image/forca4.png";
+import forca5 from "./assets/image/forca5.png";
+import forca6 from "./assets/image/forca6.png";
 
 let word = [];
 let select = "";
 let restart = false;
 
-const [stateWord, setStateWord] = useState("");
-const [error, setError] = useState(0);
-const [start, setStart] = useState(true);
-const [nivelGame, setnivelGame] = useState("");
-const [kick, setKick] = useState("");
-const [imageFirst, setImageFirst] = useState(forca0);
-
-const mix = () => Math.random() - 0.5;
-
-
 export default function App () {
+
+   const [stateWord, setStateWord] = useState("");
+   const [error, setError] = useState(0);
+   const [start, setStart] = useState(true);
+   const [nivelGame, setnivelGame] = useState("");
+   const [imageFirst, setImageFirst] = useState(forca0);
+
+   const mix = () => Math.random() - 0.5;
 
    function mixPage() {
     
@@ -34,13 +32,12 @@ export default function App () {
       setStateWord('');
       setError(0);
       setnivelGame('');
-      setKick('');
       setImageFirst(forca0);
     }
 
       playGame();
    }
-   }
+
 
    function playGame() {
       palavras.sort(mix);
@@ -52,3 +49,62 @@ export default function App () {
       }
       setStateWord(select);
    }
+
+   function letterSelected(click) {
+      
+      const startingPosition = [];
+      
+      for (let i = 0; i < word.length; i++) {
+
+        if (click === word[i]) {
+         startingPosition.push(i);
+        }
+      }
+
+      if (startingPosition.length !== 0) {
+
+        const underline = Array.from(select);
+        for (let j = 0; j < startingPosition.length; j++) {
+         underline[startingPosition[j]] = click;
+        }
+        select = underline.join("");
+        setStateWord(select);
+      } else {
+        setError(error + 1);
+        gameState();
+      }
+        finishGame();
+    }
+
+    function gameState() {
+
+      const gameImage = [
+        forca0,
+        forca1,
+        forca2,
+        forca3,
+        forca4,
+        forca5,
+        forca6,
+      ];
+      
+      setImageFirst(gameImage [error + 1]);
+      finishGame();
+    }
+
+    function finishGame() {
+
+      if (error > 5 && select !== word.join("")) {
+         setnivelGame("userLost");
+        setStart(true);
+        setStateWord(word);
+        setError(6);
+        setImageFirst(forca6);
+        restart = true;
+      } else if (select === word.join("")) {
+      setnivelGame("userWon");
+        setStart(true);
+        restart = true;
+      }
+   }
+}
